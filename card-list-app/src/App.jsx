82611a668765354f415540a0b369cd16d3f69ee2
Yarema <yaremaphoto@gmail.com>
2022-07-16 17:58:17 +0300

    create app

---
+++ card-list-app/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-no-undef */
+import './App.css';
+// import Card from './components/card';
+// import Pagination from './components/pagination copy';
+
+function App() {
+  return (
+    <div >
+      {/* <Card foo="products"/>
+      <Pagination/> */}
+      App
+    </div>
+  );
+}
+
+export default App;
